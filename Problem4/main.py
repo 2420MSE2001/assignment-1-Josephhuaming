7be9c65e552e9878d7329b76a052aad4b948a0da
Josephhuaming <2056169166@qq.com>
2025-02-20 10:22:52 +0800

    完成题目

--- Problem4/main.py
+++ Problem4/main.py
@@ -4,7 +4,11 @@ def left_side(x, num_terms):
     """
     计算方程左侧前 num_terms 项的和。
     """
-    
+    result = 0.0
+
+
+    for i in range(num_terms):
+        result += float((2**i * x**(2 ** i - 1) - 2**(i+1) * x**(2 ** (i + 1) - 1)) / (1 - x ** (2 ** i) + x ** (2 ** (i + 1))))
 
     return result
 
@@ -12,13 +16,22 @@ def right_side(x):
     """
     计算方程右侧的固定值。
     """
+
+
+    result = float((1+2*x)/(1+x+x**2))
+
     return result
 
 def find_num_terms(x, tolerance=1e-6):
     """
     寻找满足左侧与右侧差异小于容差的最小项数。
     """
-
+    
+    num_terms = 1
+    while(abs(left_side(x,num_terms) - right_side(x)) > tolerance):
+        print(left_side(x,num_terms))
+        num_terms += 1
+    
 
 
     return num_terms
